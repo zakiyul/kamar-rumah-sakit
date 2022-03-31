1fdf2ec139683ac06eea45e7955c1de3ae4f47c3
zakiul fikri <59876777+zakiyul@users.noreply.github.com>
2022-03-31 12:39:03 +0800

    add functionality

--- src/main.js
+++ src/main.js
@@ -1,14 +1,80 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import axios from 'axios';
 
+import './web-components/card-component.js';
+
 const baseUrl = 'https://rs-bed-covid-api.vercel.app/api';
+const selectProvinsi = document.getElementById('pilih-provinsi');
+const selectKabupaten = document.getElementById('pilih-kabupaten');
+const btnCari = document.getElementById('btn-cari')
+
+let provinsiId = '';
+let hospitals = [];
+
+const getDataHospital = (provinsiId, kabupatenId) => {
+    axios.get(`${baseUrl}/get-hospitals?provinceid=${provinsiId}&cityid=${kabupatenId}&type=1`)
+     .then(res => {
+        console.log(res.data);
+        hospitals = res.data.hospitals;
+     })
+     .catch(err => {
+         console.log(err)
+     })
+}
+
+const getDataKabupaten = provinsiId => {
+    axios.get(`${baseUrl}/get-cities?provinceid=${provinsiId}`)
+     .then(res => {
+         const kabupatens = res.data.cities;
+         console.log(kabupatens)
+         kabupatens.map(kabupaten => {
+             const kabupatenOption = document.createElement('option');
+             kabupatenOption.setAttribute('value', kabupaten.id);
+             kabupatenOption.innerText = kabupaten.name;
+             selectKabupaten.appendChild(kabupatenOption);
+         })
+         console.log('ubah jadi enable')
+         selectKabupaten.removeAttribute('disabled')
+     })
+     .catch(err => {
+         console.log(err.message)
+     })
+}
 
 const getDataProvinsi = () => {
     axios.get(`${baseUrl}/get-provinces`)
-     .then(res => console.log(res.data))
+     .then(res => {
+         console.log(res.data)
+         const provinces = res.data.provinces;
+         provinces.map(province => {
+             const provinsiOption = document.createElement('option');
+             provinsiOption.setAttribute('value', province.id);
+             provinsiOption.innerText = province.name;
+             selectProvinsi.appendChild(provinsiOption)
+         })
+    })
      .catch(err => console.log(err))
 }
 
+selectProvinsi.addEventListener('change', e => {
+    getDataKabupaten(e.target.value);
+    provinsiId = e.target.value;
+})
+
+selectKabupaten.addEventListener('change', e => {
+    getDataHospital(provinsiId, e.target.value);
+})
+
+btnCari.addEventListener('click', e => {
+    console.log(hospitals)
+    hospitals.map(hospital => {
+        const cardItemElement = document.createElement('card-component');
+        cardItemElement.konten = hospital;
+        document.getElementById('card-component').appendChild(cardItemElement);
+    })
+})
+
 document.addEventListener("DOMContentLoaded", e => {
-    getDataProvinsi()
+    getDataProvinsi();
+    selectKabupaten.setAttribute('disabled', true);
 })
--- src/web-components/card-component.js
+++ src/web-components/card-component.js
@@ -13,7 +13,7 @@ class CardComponent extends HTMLElement {
                 <div className="card-body">
                     <h5 className="card-title">${this._konten.name}</h5>
                     <p className="card-text">${this._konten.address}</p>
-                    <a href="#" className="btn btn-primary">${this._konten.bed_availability}</a>
+                    <div href="#" className="bg-primary">${this._konten.bed_availability}</div>
                 </div>
             </div>  
         `
